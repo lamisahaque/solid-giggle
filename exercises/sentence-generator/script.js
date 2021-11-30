e23fa9f326f23c34400057eb92da2787583cab3b
lamisahaque <lamisa.h@columbia.edu>
2021-11-30 15:57:06 -0500

    add

---
+++ exercises/sentence-generator/script.js
@@ -0,0 +1,12 @@
+
+console.log ("it works!")
+
+let nouns = ["heart", "rainbow", "ocean "]; 
+let verbs = ["look", "make", "continure"]; 
+let adjectives = ["good", "different", "possible"]; 
+
+
+ // let item = arrayName [Math.random(Math.floor)*arrayName.length)];
+
+ let noun = nouns [Math.random(Math.floor) * nouns.length)];
+
