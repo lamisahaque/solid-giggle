989bcfced3918b73aba714c4152323f50d6db365
lamisahaque <lamisa.h@columbia.edu>
2021-11-30 15:55:56 -0500

    add

---
+++ exercises/intro-to-javascript/script-02.js
@@ -0,0 +1,12 @@
+
+
+let age= 12; 
+if (age <8){
+    console, log ("Check out the Merry-Go-Round. You'll love it!");
+
+}else if )age > 8 && age < 65) {
+    console.log("Chekc out the Roller coaster. It's awesome!");
+
+}else {
+    console log. ("why not enjoy a float down the lazy river?");
+}
