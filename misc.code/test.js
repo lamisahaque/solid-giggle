bd7a27512ff7134b6fdbe24fa92db8e63234f3c2
lamisahaque <lamisa.h@columbia.edu>
2021-11-30 19:32:37 -0500

    yes

---
+++ misc.code/test.js
@@ -0,0 +1,11 @@
+let container = document.querySelector(".container");
+
+// create the image
+let image = document.createElement("img");
+image.setAttribute(
+  "src",
+  "https://www.leapingbunny.org/sites/default/files/brown-bunny.png"
+);
+
+// append the image to the container
+container.appendChild(image);
