13d183f2f387e060f4293ce2f569cb646b80e05b
lamisahaque <lamisa.h@columbia.edu>
2021-12-22 20:37:47 -0500

    y

--- exercises/exercise-08/script.js
+++ exercises/exercise-08/script.js
@@ -1,5 +1,6 @@
 /* 
-Instructions: when a user clicks on one of the colored dots, the background color of the entire page changes to match that dot. You should not need to change any HTML or CSS. 
+Instructions: when a user clicks on one of the colored dots, the background color of the entire page changes to match that dot. You should
+ not need to change any HTML or CSS. 
 Hint: Classes for each color have already been defined in the CSS tab.
 */
 let text = document.querySelector("body");
@@ -22,6 +23,7 @@ let redButton = document.getElementById("redButton");
 let greenButton = document.getElementById("greenButton");
 let blueButton = document.getElementById("blueButton");
 let yellowButton = document.getElementById("yellowButton");
+let resetButton = document.getElementById("resetButton");
 
 function redScheme() {
   document.body.style.backgroundColor = "#ffaaaa";
@@ -43,7 +45,13 @@ function yellowScheme() {
   document.getElementById("text").style.color = "rgb(255, 255, 162)";
 }
 
+function resetScheme() {
+  document.body.style.bagroundColor = "rgb(255, 255, 255)";
+  document.getElementById("text").style.color = "rgb(255, 255, 255)";
+}
+
 redButton.addEventListener("click", redScheme);
 greenButton.addEventListener("click", greenScheme);
 blueButton.addEventListener("click", blueScheme);
 yellowButton.addEventListener("click", yellowScheme);
+resetButton.addEventListener("click", resetScheme);
